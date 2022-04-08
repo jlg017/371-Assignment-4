2aa6f603ca1d1ba2b826f984e48af44f9c779740
Jackie Greer <jlg17@sfu.ca>
2022-04-08 10:14:04 -0700

    fixed bug in forwardToRow() where non-matching address would break everything

--- #371 Assignment 4 Question 3.py
+++ #371 Assignment 4 Question 3.py
@@ -1,15 +1,4 @@
 #371 Assignment 4 Question 3
-#TODO:
-
-#For each pkt to forward:
-    # read IP dest of pkt to fwd - done
-    # convert IP dest to binary - done
-    # implement fwding algorithm -> calculations done in binary
-    # include use of metric
-    # print(The destination IP address is <IP address in x.x.x.x format> )
-    # print(The next hop IP address is <IP address in x.x.x.x format> )
-    # print(The port the packet will leave through is <int port #>)
-
 
 #Helper Function to 'bitwiseAND' and 'addressToBinaryString' Methods
     #Paramter: A decimal number
@@ -90,7 +79,6 @@ def bitwiseAND (add1, add2):
     #split addresses into list of 4 octets
     convertAdd1 = splitAddress(add1)
     convertAdd2 = splitAddress(add2)
-
     newAddress = ""
 
     #Perform Bitwise AND for each octet set
@@ -118,13 +106,12 @@ def countOnes(str):
 
     return count
 
-#Forwarding algorithm ##TODO: delete print statements when done debugging | Date modified: April 7, 12PM
+#Forwarding algorithm 
     #Parameters:
         #table: Linear list of addresses and masks
         #destIP: A single string of 32 0s and 1s
     #Return: The row to forward address to; in form of an item from a list
 def forwardToRow(table, destIP):
-    print("bit destination IP address = "+ destIP)
     #mask length of rowMatch
     longestMatch = 0
     #rowMatch = row that matches, get forwarding information from
@@ -135,35 +122,24 @@ def forwardToRow(table, destIP):
         mask = row[2]
         metric = row[3]
         #bitwise AND mask and destIP
-        #netID = bAND(mask, destIP)
         netID = bitwiseAND(mask,destIP)
-
         #compare result to row destination IP
         if(netID == rDest):
-            print("netID: "+ netID +" == rDest: "+ rDest)
-
-            #find length of the mask
+            #want longest mask for match
             lengthMatch = countOnes(mask)
-            print("length of match's mask = " + str(lengthMatch))
-
             if(lengthMatch > longestMatch):
-                print("new longestMatch, changing rowMatch")
                 rowMatch = row
                 longestMatch = lengthMatch
-
-            elif(lengthMatch == longestMatch): 
-                print("match found of same length")
-                #choose lowest metric
-                print("metric ="+str(metric)+", rowMatchMetric = "+str(rowMatch[3]))
-
-                if(int(metric) < int(rowMatch[3])):
-                    print("metric < previous rowMatch, changing rowMatch")
-                    rowMatch = row 
-                else: #if metric =>  no change to rowMatch
-                    print("no change to rowMatch")
-                    continue
-        else:
-            print("netID: "+ netID +" != rDest: "+ rDest)
+            elif(lengthMatch == longestMatch):   
+                #check if rowMatch = default
+                if(rowMatch == -1):
+                    rowMatch = row
+                else:
+                    #choose lowest metric
+                    if(int(metric) < int(rowMatch[3])):
+                        rowMatch = row 
+                    else: #if metric =>  no change to rowMatch
+                        continue
     
     return rowMatch
 
@@ -177,102 +153,101 @@ def printRTable(rTable):
     print("]")
     return
 
-########## MAIN 
-
-finished = False
-
-while (finished != True):
-    ##1. Parse information from forwarding table file-----------------------------------------------------------
-    print("1. Getting information from user ...")
-    destIP = input("Enter the IP address for your packet destination:\n")
-    #created file with test routing table information | Apr 4th 
-    
-    #TODO: change below to take in table (before submission)
-    # #table = input("Enter the file name for your forwarding table:\n")
-    
-    fTable = open('forwardTableTest1.txt', 'r')
+#Reads in and sorts table from file 
+    #Parameter: file name for forwarding table
+    #Return: linear list of items (table sorted by mask size)
+def ingestAndSort(table):
+    #read file contents
+    fTable = open(table, 'r')
     rFileLines = []
     rFileLines = fTable.readlines()
 
-    ##2. Print Routing Table (Sorted) to screen-----------------------------------------------------------
-    print("\n\n2. Create Routing Table (Sorted) ")
-
     # Order routing table by mask length: longest -> shortest
     rFileLines.sort(reverse=True, key = lambda x: x[2])
-    print("Sorting table ...\n")
-    
     rTableRows = []
     
     for line in rFileLines:
         #splits line into list of entries separated by tab
         rTableRow = line.split("\t")
-
         #Strips '\n' from the end of Interface item | April 6th - make more sophisticated | April 7 changed to strip() instead of replace()
         temp = rTableRow[len(rTableRow)-1].strip()
         rTableRow[len(rTableRow)-1] = temp
         rTableRows.append(rTableRow)
-    
-    printRTable(rTableRows)
 
-    ##3. Convert Addresses to Binary 8-bits-----------------------------------------------------------
-    print("\n\n3. Converting addresses to binary 8-bits ... ")
-    
-    ## convert destination address
-    destIPBin = addressToBinaryString(destIP)
+    return rTableRows
 
-    ## convert routing table
+#Converts Routing Table IP addresses to binary strings & strips port number from interface
+    #Parameter: A linear list of items
+    #Return: binary addressed linear list of items
+def tableToBin(rTableRows):
     rTableBin = []
     for row in rTableRows:
-        #convert row of addresses to binary
         rowBin = []
         for i in range(0, len(row)):
             if i == 0: # Network address 
                 netAddrBin = addressToBinaryString(row[i])
                 rowBin.append(netAddrBin)
-
             elif i == 1: # Gateway address
                 if row[i] == '*':
                     rowBin.append('*')
                 else:                    
                     gateAddrBin = addressToBinaryString(row[i])
                     rowBin.append(gateAddrBin)
-
             elif i == 2: # Mask
                 maskAddrBin = addressToBinaryString(row[i])
                 rowBin.append(maskAddrBin)
-
             elif i == 3: # Metric
-                rowBin.append(row[i])\
-
+                rowBin.append(row[i])
             elif i == 4: # Interface
                 #extract port number from eth#, convert to int
-                interface = int(row[i][len(row[i])-1])
-                rowBin.append(interface)
-
+                portNum = int(row[i][len(row[i])-1])
+                rowBin.append(portNum)
             else:
                 print("error: ")
                 print(row[i])
+        #add row to table        
         rTableBin.append(rowBin)
+
+    return rTableBin
+
+
+########## MAIN ##########
+
+finished = False
+
+##1. Parse information from forwarding table file ##
+print("1. Getting table information from user ...") #---------------------------keep?
+table = input("Enter the file name for your forwarding table:\n")
+rTableRows = ingestAndSort(table)
+
+##2. Print Sorted Routing Table to screen ##
+print("\n\n2. Sorted Routing Table:") #---------------------------keep number?
+printRTable(rTableRows)
+#convert table to binary
+rTableBin = tableToBin(rTableRows)
+
+## LOOP ##
+while (finished != True):
+
+    ##3. Parse information from forwarding table file ##
+    print("3. Getting IP Address information from user ...") #---------------------------keep?
+    destIP = input("Enter the IP address for your packet destination:\n")
+
+    ##4. Convert Destination Address to Binary 8-bits ## 
+    print("\n\n4. Converting address to binary 8-bits ... ") #---------------------------keep?
+    destIPBin = addressToBinaryString(destIP)
+    print(destIPBin) #---------------------------keep?
     
-    # table with addresses converted to binary 
-    print("rTableBin: (do not print in final implementation)")
-    printRTable(rTableBin)
-
-    ##4. Forwarding Part-----------------------------------------------------------
-    print("\n\n4. Forwarding addresses ...")
-
-    #Variables
+    ##5. Forwarding Part ##
+    print("\n\n5. Forwarding addresses ...") #---------------------------keep?
 
     #Algorithm begins here
     rowChosen = forwardToRow(rTableBin, destIPBin)
-    print("row using = "+ str(rowChosen))
     nextHopIP = ""
-    port = int(rowChosen[4])
+    port = rowChosen[4]
 
-    #TODO: convert binary addresses back to IP address
+    #convert binary addresses back to IP address and determine if next hop or destination
     netAddr = binToIP(rowChosen[0])
-
-    #determine if next hop or destination
     if(str(rowChosen[1]) == '*'):
         nextHopIP = netAddr
         dest = destIP
@@ -284,13 +259,12 @@ while (finished != True):
     print("The next hop IP address is "+ nextHopIP)
     print("The port the packet will leave through is "+ str(port))
 
-    ##5 After Forwarding: Ask for User Input - change Y,N option?-----------------------------------------------------------
-    print("\n\n5. Continue?")
+    ##6. After Forwarding: Ask for User Input re: continue ##
     formatCorrect = False
 
     while (formatCorrect != True):
         print("Would you like to forward another packet?")
-        userAnswer = input("Enter 'Y' for yes, 'N' for no : ")
+        userAnswer = input("Enter 'Y' for yes, 'N' for no: ")
         parsedAnswer = userAnswer.capitalize()
 
         if(parsedAnswer == 'N'):
